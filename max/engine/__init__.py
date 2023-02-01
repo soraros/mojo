d2b13550bc87060b1cd97049b5dd0fb092678cf0
Aditya Bhagwat <adityab@modular.com>
2023-02-01 12:44:13 -0800

    [Python API] Rename modular.pyframework to modular.engine (#8054)
    Addresses #8029
    
    MAX_PYTHON_ORIG_REV_ID: 67c20b4844d4e4a81b8c3fc3212a9352b154d4e5

--- max/engine/__init__.py
+++ max/engine/__init__.py
@@ -4,7 +4,7 @@
 #
 # ===----------------------------------------------------------------------=== #
 
-import modular.pyframework.core as mdcore
-from modular.pyframework.core import InferenceSession, Model
+import modular.engine.core as mdcore
+from modular.engine.core import InferenceSession, Model
 
 __version__ = mdcore.__version__
