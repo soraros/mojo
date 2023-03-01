a20a0c743449b5e78e32993ecd567249ea03fe20
Aditya Bhagwat <adityab@modular.com>
2023-03-01 10:39:04 -0800

    [API] Add Python binding for input shapes and number of inputs (#9199)
    This PR wraps underlying C model metadata functions in Python to inspect number of input and output tensors or input tensors' shapes. Closes #8021.
    
    MAX_PYTHON_ORIG_REV_ID: 9c815d0f846ea6d900ba583c8e2eed027d1fead7

--- max/_core/engine.pyi
+++ max/_core/engine.pyi
@@ -4,6 +4,12 @@
 #
 # ===----------------------------------------------------------------------=== #
 from pathlib import Path
+from sys import version_info
+
+if version_info.minor <= 8:
+    from typing import List
+else:
+    List = list
 
 class Model:
     def execute(self, *args) -> None: ...
@@ -12,3 +18,6 @@ class Model:
 class InferenceSession:
     def __init__(self, config: dict = ...) -> None: ...
     def compile(self, model_path: Path, config: dict = ...) -> Model: ...
+
+class TensorSpec:
+    def shape(self) -> List[int]: ...
