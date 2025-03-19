031fff7a61d16f2487d272f172467e9bcd93747b
Stef Lindall <stef@modular.com>
2025-03-18 17:30:36 -0700

    [SDK] Load Modular dialects for all mlir contexts
    Now that we can share MLIR objects between the MLIR C API bindings
    safely, we can load all Modular MLIR dialects into the context by
    default!
    
    You can now create a context normally and access all Modular dialects
    with `from max.mlir import Context; ctx = Context()`.
    
    MAX_PYTHON_ORIG_REV_ID: 35144a11a16c93545427ab8973b6454a65cefc73

--- max/mlir/__init__.py
+++ max/mlir/__init__.py
@@ -4,8 +4,16 @@
 #
 # ===----------------------------------------------------------------------=== #
 
-from max._mlir._mlir_libs._mlir import *
-from max._mlir._mlir_libs._mlir.ir import *
-from max._mlir._mlir_libs._mlir.ir import (  # type: ignore[attr-defined,no-redef]
-    _BaseContext as Context,
-)
+from .._mlir._mlir_libs._mlir import *
+from .._mlir._mlir_libs._mlir.ir import *
+
+
+def site_initialize():
+    from max._core import graph  # type: ignore
+
+    from .._mlir._mlir_libs import get_dialect_registry
+
+    graph.load_modular_dialects(get_dialect_registry())
+
+
+site_initialize()
