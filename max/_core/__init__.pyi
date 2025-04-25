a246c2e495969365f19f810cfb8b0e33e6bd248d
Stef Lindall <stef@modular.com>
2025-04-25 11:25:51 -0700

    [SDK] Bind a bunch more built in types and functions (#59808)
    Stacked PRs:
     * #59809
     * __->__#59808
    
    --- --- ---
    
    ### [SDK] Bind a bunch more built in types and functions
    
    MAX_PYTHON_ORIG_REV_ID: f06f65bc0fd480eada3400933a86567291f567ad

--- max/_core/__init__.pyi
+++ max/_core/__init__.pyi
@@ -6,8 +6,15 @@
 # GENERATED FILE, DO NOT EDIT MANUALLY!
 # ===----------------------------------------------------------------------=== #
 
-from max._mlir._mlir_libs._mlir import MlirValue  # type: ignore
+import enum
+from collections.abc import Sequence
+from typing import Callable, Generic, TypeVar
+
+from max.mlir import Attribute as MlirAttribute
+from max.mlir import Block as MlirBlock
 from max.mlir import Location
+from max.mlir import Type as MlirType
+from max.mlir import Value as MlirValue
 
 from . import (
     dialects as dialects,
@@ -28,34 +35,84 @@ from . import (
     profiler as profiler,
 )
 
+OpType = TypeVar("OpType", bound=Operation)
+
 class Attribute:
+    def __init__(self, arg: MlirAttribute, /) -> None: ...
     def __eq__(self, arg: object, /) -> bool: ...
+    @property
+    def asm(self) -> str: ...
+    def __repr__(self) -> str: ...
+    @property
+    def _CAPIPtr(self) -> object: ...
 
 class Block:
+    def __init__(self, arg: MlirBlock, /) -> None: ...
     @property
     def end(self) -> InsertPoint: ...
 
 class InsertPoint:
     pass
 
+class NamedAttribute:
+    def __init__(self, arg0: str, arg1: Attribute, /) -> None: ...
+    @property
+    def name(self): ...
+    @property
+    def value(self) -> Attribute: ...
+    def __iter__(self) -> tuple: ...
+
 class OpBuilder:
     def __init__(self, arg: InsertPoint, /) -> None: ...
     def create(
-        self, arg0: type[OpState], arg1: Location, /, *args, **kwargs
-    ) -> object: ...
+        self, type: OpType, location: Location
+    ) -> Callable[..., OpType]: ...
 
-class OpState:
-    pass
-
-class Type:
+class Operation:
     def __eq__(self, arg: object, /) -> bool: ...
     @property
-    def ctype(self) -> object: ...
+    def results(self) -> Sequence[Value]: ...
+    @property
+    def asm(self) -> str: ...
+    def __repr__(self) -> str: ...
+    @property
+    def _CAPIPtr(self) -> object: ...
 
-class Value:
+T = TypeVar("T", bound=Type)
+
+class Type:
+    def __init__(self, arg: MlirType, /) -> None: ...
+    def __eq__(self, arg: object, /) -> bool: ...
+    @property
+    def asm(self) -> str: ...
+    def __repr__(self) -> str: ...
+    @property
+    def _CAPIPtr(self) -> object: ...
+
+class TypeID:
+    pass
+
+class Value(Generic[T]):
     def __init__(self, arg: MlirValue, /) -> None: ...
     def __eq__(self, arg: object, /) -> bool: ...
     @property
     def type(self) -> Type: ...
+    @property
+    def _CAPIPtr(self) -> object: ...
+
+class _BitVector:
+    pass
+
+class _LockedSymbolTableCollection:
+    pass
+
+class _Operation:
+    pass
+
+class _RelocationModel(enum.Enum):
+    pass
+
+class _TargetTriple:
+    pass
 
 __version__: str
