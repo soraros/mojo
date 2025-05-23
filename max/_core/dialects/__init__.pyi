a3c96a6c402db8a854382f30c041f7781627bd13
Stef Lindall <stef@modular.com>
2025-05-22 17:14:24 -0700

    [SDK] `rmo` dialect bindings for python
    MAX_PYTHON_ORIG_REV_ID: eb91f215272466bdbf6988a02f4d4325dcf09186

--- max/_core/dialects/__init__.pyi
+++ max/_core/dialects/__init__.pyi
@@ -6,8 +6,21 @@
 # GENERATED FILE, DO NOT EDIT MANUALLY!
 # ===----------------------------------------------------------------------=== #
 
-from . import builtin as builtin
-from . import kgen as kgen
-from . import m as m
-from . import mo as mo
-from . import mosh as mosh
+from . import (
+    builtin as builtin,
+)
+from . import (
+    kgen as kgen,
+)
+from . import (
+    m as m,
+)
+from . import (
+    mo as mo,
+)
+from . import (
+    mosh as mosh,
+)
+from . import (
+    rmo as rmo,
+)
