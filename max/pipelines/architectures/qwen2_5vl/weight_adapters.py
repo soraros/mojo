35f99d09962a3ea36db17cf98906cb6b1f89c014
Aisha Mohamed <42137324+aishahasmoh@users.noreply.github.com>
2025-09-29 14:25:10 -0500

    [Models] Cast Qwen2.5VL vision encoder weights to bf16
    Currently, our optimized kernels use bf16. Transformers uses fp32
    for the vision encoder only for the 32B model. Other models (3B, 7B,
    and 72B) models use bf16.
    
    MODULAR_ORIG_COMMIT_REV_ID: 9cd8bb3f50a9ba33ceef3a6d0f3c134ebb94145a

--- max/pipelines/architectures/qwen2_5vl/weight_adapters.py
+++ max/pipelines/architectures/qwen2_5vl/weight_adapters.py
@@ -14,6 +14,7 @@
 from __future__ import annotations
 
 from max.driver import Tensor
+from max.dtype import DType
 from max.graph.weights import WeightData, Weights
 
 # Maps from Qwen2.5VL checkpoint names to Qwen2.5VLLanguageModel weight names.
@@ -53,16 +54,19 @@ def convert_qwen2_5vl_model_state_dict(
     llm_state_dict: dict[str, WeightData] = {}
 
     for checkpoint_name, weight in state_dict.items():
+        if weight.data().dtype != DType.bfloat16:
+            weight_data = weight.data().astype(DType.bfloat16)
+        else:
+            weight_data = weight.data()
         # Special case for lm_head. Because config.tie_word_embeddings is true
         # for some Qwen2.5VL models and false for others.
         if checkpoint_name.startswith("lm_head."):
             llm_name = checkpoint_name.replace(
                 "lm_head.", "language_model.lm_head."
             )
-            llm_state_dict[llm_name] = weight.data()
+            llm_state_dict[llm_name] = weight_data
         elif "patch_embed.proj." in checkpoint_name:
             # Convert Conv3D weight to a Linear-equivalent format. MAX uses Linear layer instead of Conv3D for patch embedding.
-            weight_data = weight.data()
             weight_array = Tensor.from_dlpack(weight_data.data)
             out_channels, in_channels, kernel_h, kernel_w, kernel_d = (
                 weight_array.shape
@@ -88,6 +92,6 @@ def convert_qwen2_5vl_model_state_dict(
             for before, after in QWEN2_5_VL_MODEL_MAPPING.items():
                 llm_name = llm_name.replace(before, after)
 
-            llm_state_dict[llm_name] = weight.data()
+            llm_state_dict[llm_name] = weight_data
 
     return llm_state_dict
