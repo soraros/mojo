[SDK] Fix QwenVL off-by-one text-only decoder pos id
Fix an off-by-one error in computing the decoder position IDs for
Qwen 2.5 VL.
The third branch of this code is re-computing decoder position IDs but
was accidentally using current_length as the offset -- but
ctx.current_length at this point includes the active token as well.
So use `ctx.start_idx` instead to fix the off by one.

This was surfaced by 0ca0f2ec87 but seems to have been present
previously.
I think the code path was only hit in prefill and was always incorrect
for decode.

Testing:
- Unit test checking that the first decode's pos ID is prompt size + 1.
MODULAR_ORIG_COMMIT_REV_ID: 3bace5bca4498d498e94719e872a93623e715c02

max/pipelines/architectures/qwen2_5vl/model.py
@@ -1061,7 +1061,8 @@ class Qwen2_5VLModel(PipelineModel[TextAndVisionContext], KVCacheMixin):
                     temp_position_ids = temp_position_ids.reshape(1, 1, -1)
                     temp_position_ids = np.tile(temp_position_ids, (3, 1, 1))
                     delta = (
-                        ctx.current_length
+                        # Offset by the number of previous tokens (start_idx).
+                        ctx.start_idx
                         + ctx.extra_model_args["rope_delta"].item()
                     )
                     temp_position_ids = temp_position_ids + delta